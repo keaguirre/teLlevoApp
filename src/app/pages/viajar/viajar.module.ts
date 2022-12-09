3a0dfa0679c01d0ca9f9b5f3f4aa9ec070d52914
keaguirre <ke.aguirre@duocuc.cl>
2022-12-08 23:29:23 -0300

    Pagina de viajes y creacion de solicitudes

--- src/app/pages/viajar/viajar.module.ts
+++ src/app/pages/viajar/viajar.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ViajarPageRoutingModule } from './viajar-routing.module';
@@ -12,6 +11,7 @@ import { ComponentsModule } from '../../components/components.module'
   imports: [
     CommonModule,
     ComponentsModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
     ViajarPageRoutingModule
--- src/app/pages/viajar/viajar.page.ts
+++ src/app/pages/viajar/viajar.page.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, AlertController, LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-viajar',
@@ -7,11 +9,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viajar.page.scss'],
 })
 export class ViajarPage implements OnInit {
+private listHidden: boolean = false;
+private listHidden1: boolean = true;
+onDestination :FormGroup;
+usr:any;
+comunas: any = [
+    {p_comuna:'San Joaquin'},
+    {p_comuna:'Macul'},
+    {p_comuna:'La Granja'},
+    {p_comuna:'Santiago'}
+];
 
-  constructor() { }
+  constructor(private toast:ToastController ,private loadingCtrl:LoadingController, private formBuilder:FormBuilder) { }
 
 
   ngOnInit() {
+    document.getElementById("list").hidden = false;
+    this.onForm();
   }
 
+  onQuehue(){//si el formulario es valido, agregar usr-logged al form
+    if(this.onDestination.valid){
+      this.usr = localStorage.getItem('logged-usr')
+      Object.assign(this.onDestination.value,{p_email:this.usr})
+      this.onDestination.reset();
+    }
+    else{
+      //submit vacio alerta o algun feedback
+      this.presentToast();
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toast.create({
+      message: 'Error, Ingrese todos los campos',
+      duration: 2000,
+      cssClass: 'custom-toast',
+      icon:'alert'
+    });
+    await toast.present();
+  }
+
+  onForm(){
+    this.onDestination = this.formBuilder.group({
+      p_email: new FormControl('default@email.com', [Validators.required]),
+      p_comuna: new FormControl('', [Validators.required]),
+      p_calle: new FormControl('', [Validators.required, Validators.maxLength(32)]),
+      p_acompanantes: new FormControl('0', [Validators.required]),
+    });
+  }
+
+  onSearch(){
+    //form
+    if(this.listHidden === false){
+      this.listHidden = true;
+      document.getElementById("list").hidden = true;
+    }
+    else if(this.listHidden === true){
+      this.listHidden = false;
+      document.getElementById("list").hidden = false;
+    }
+    //loader
+    if(this.listHidden1 === true){
+      this.listHidden1 = false;
+      document.getElementById("loader").hidden = false;
+    }
+    else if(this.listHidden1 === false){
+      this.listHidden1 = true;
+      document.getElementById("loader").hidden = true;
+    }
+  };
 }
